3817ee02bd7d2164dd36f27d70b4aa0a6e3a1794
sekian <anass.benali@gmail.com>
2022-09-04 20:25:35 +0200

    Added kafka postback for each data object

--- ingestion-agent/ingest.php
+++ ingestion-agent/ingest.php
@@ -1,8 +1,41 @@
 <?php
+    require 'vendor/autoload.php';
+    const BROKER_URI = '127.0.0.1:9092';
+    const TOPIC = 'postback';
+    const VERSION = '3.2.1';
+    const REFRESH_MS = 10000;
+    
 	$POST = json_decode(file_get_contents("php://input"), true);
-    $endpoint = $POST["endpoint"];
-    $data = $POST["data"];
+    
+    if(!isset($POST['endpoint']) || !isset($POST['data'])) {
+        echo 'ERROR: Message body is missing "endpoint" or "data"';
+        exit();
+    }
+    
+    $endpoint = $POST['endpoint'];
+    $data = $POST['data'];
+	// var_dump($endpoint) . PHP_EOL;
+	// var_dump($data) . PHP_EOL;
+    
+    $config = \Kafka\ProducerConfig::getInstance();
+    $config->setMetadataRefreshIntervalMs(REFRESH_MS);
+    $config->setMetadataBrokerList(BROKER_URI);
+    $config->setRequiredAck(1);
+    $config->setIsAsyn(false);
+    $config->setProduceInterval(500);
+    $producer = new \Kafka\Producer();
+    
+    foreach((array) $data as $value) {
+        $value['method'] = $endpoint['method'] ?? null;
+        $value['url'] = $endpoint['url'] ?? null;
+        print_r($value) . PHP_EOL;
 
-	echo print_r($endpoint) . PHP_EOL;
-	echo print_r($data) . PHP_EOL;
+        $producer->send([
+            [
+                'topic' => TOPIC,
+                'value' => json_encode($value),
+                'key' => '',
+            ],
+        ]);
+    }
 ?>
